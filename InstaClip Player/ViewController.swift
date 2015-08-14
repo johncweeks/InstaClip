f3e5a14ed5e615f52087de03dc58073738afa748
johncweeks <john@soundscope.co>
2015-08-14 12:16:42 -0400

    Initial Commit

---
+++ InstaClip Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaClip Player
+//
+//  Created by John Weeks on 8/14/15.
+//  Copyright © 2015 Moonrise Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ InstaClip PlayerTests/InstaClip_PlayerTests.swift
@@ -0,0 +1,36 @@
+//
+//  InstaClip_PlayerTests.swift
+//  InstaClip PlayerTests
+//
+//  Created by John Weeks on 8/14/15.
+//  Copyright © 2015 Moonrise Software. All rights reserved.
+//
+
+import XCTest
+@testable import InstaClip_Player
+
+class InstaClip_PlayerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
